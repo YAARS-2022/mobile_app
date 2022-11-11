87e909e80aadfb47ff12c86873ac0d8621d10745
Ashmit Rajaure <ashmit.rajaure@gmail.com>
2022-11-11 18:52:12 +0545

    Store the received data into the object.
    See: #4

--- lib/data/firestore_helper.dart
+++ lib/data/firestore_helper.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:yaars/models/bus_data.dart';
 
 class FSHelper {
   static late FirebaseFirestore _db;
@@ -10,8 +11,8 @@ class FSHelper {
   static getData() async {
     await _db.collection("Buses").get().then((event) {
       for (var doc in event.docs) {
-        print("Doc ID: ${doc.id}");
-        print("Data : ${doc.data()}");
+        var busData = BusData.fromMap(doc.data());
+        print(busData);
       }
     });
   }
