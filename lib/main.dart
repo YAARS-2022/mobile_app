645ce12d33ec58869ea127b9cd2f55bdfb278fa0
Ashmit Rajaure <ashmit.rajaure@gmail.com>
2022-11-12 02:19:44 +0545

    Save child using GetStorage
    See: #7

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:yaars/data/firestore_helper.dart';
 import 'package:yaars/home.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -19,6 +21,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(GetStorage().read('child'));
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
