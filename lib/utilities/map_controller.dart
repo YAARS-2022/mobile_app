1084cea8c3108f36be09d143071dad3067f6a482
Ashmit Rajaure <ashmit.rajaure@gmail.com>
2022-11-21 15:05:45 +0545

    Draw road from the bus to parent

--- lib/utilities/map_controller.dart
+++ lib/utilities/map_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:yaars/data/bus_data_controller.dart';
 import 'dart:developer' as developer;
@@ -26,6 +27,7 @@ class RouteController extends GetxController{
       developer.log("Setting the map controller", name: "RouteController");
 
       GeoPoint? childBusLocation = await BusDataController.getBusLocation(name);
+      GeoPoint? userLocation = await getUserLocation();
 
       if(childBusLocation == null){
         Get.snackbar(
@@ -37,21 +39,39 @@ class RouteController extends GetxController{
         // Add child's bus marker
 
         await mapController.value.addMarker(childBusLocation, markerIcon: childMarkerOnMap);
-
-        var newMapController = MapController(
-          initMapWithUserPosition: true,
-          areaLimit: BoundingBox(
-            east: 10.4922941,
-            north: 47.8084648,
-            south: 45.817995,
-            west: 5.9559113,
+        RoadInfo roadInfo = await mapController.value.drawRoad(
+          childBusLocation,
+          userLocation,
+          roadType: RoadType.car,
+          roadOption: const RoadOption(
+            roadWidth: 10,
+            roadColor: Colors.blue,
+            showMarkerOfPOI: false,
+            zoomInto: true,
           ),
         );
-
-        await newMapController.addMarker(childBusLocation, markerIcon: childMarkerOnMap);
-
-        mapController.value = newMapController;
       }
 
     }
+
+  Future<GeoPoint> getUserLocation() async {
+    bool locationServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    LocationPermission permission;
+
+    if(!locationServiceEnabled){
+      return Future.error('Location services disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied){
+      return Future.error('Location permissions are denied');
+    }
+
+    Position userLocation = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    var parentLat = userLocation.latitude;
+    var parentLon = userLocation.longitude;
+
+    return GeoPoint(latitude: parentLat, longitude: parentLon);
+  }
 }
